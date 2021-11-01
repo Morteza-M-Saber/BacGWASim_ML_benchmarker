8c915e1cf91ee2acb7afa0deba71bd2046f3402f
Morteza-M-Saber <morteza.mahmoudisaber@gmail.com>
2021-10-31 20:40:13 -0400

    feature_selection_classes

--- src/modules/python_classes/ploter_class.py
+++ src/modules/python_classes/ploter_class.py
@@ -5,26 +5,37 @@ Created on Mon Jun  1 21:45:17 2020
 @author: Masih
 ای که خواهان تولدی دیگری, نخست مرگ را پذیرا باش
 """
-#convert cm to inch
+# convert cm to inch
 def cm2inch(*tupl):
     inch = 2.54
     if isinstance(tupl[0], tuple):
-        return tuple(i/inch for i in tupl[0])
+        return tuple(i / inch for i in tupl[0])
     else:
-        return tuple(i/inch for i in tupl)
-    
+        return tuple(i / inch for i in tupl)
+
+
 class box_ploter:
-    
     def __init__(self):
 
         pass
-        
-    def dot_box_plot(self,df,x,y,out,hue=False,ylabel='',xlabel='',
-                     label_median=True,showmeans=True,
-                     showfliers=False,figsize = (8, 8)):
+
+    def dot_box_plot(
+        self,
+        df,
+        x,
+        y,
+        out,
+        hue=False,
+        ylabel="",
+        xlabel="",
+        label_median=True,
+        showmeans=True,
+        showfliers=False,
+        figsize=(8, 8),
+    ):
         """
-        
-    
+
+
         Parameters
         ----------
         df : TYPE
@@ -49,61 +60,96 @@ class box_ploter:
             Show the outliers in boxplot along with dot plot. The default is False.
         figsize: (x,y),optional.
             Size of the plot. The default is (8,8) in centimeters.
-    
+
         Returns
         -------
         the boxplot figure.
-        
+
         Example:
         tips = sns.load_dataset("tips")
         box_ploter(df=tips,x="day", y="total_bill",out='boxplot_test.png')
-    
+
         """
-        import seaborn as sns
         import matplotlib.pyplot as plt
-        from pylab import plot, show, savefig, xlim, figure,  ylim, legend, boxplot, setp, axes
         import numpy as np
-        plt.figure(figsize = cm2inch(figsize))
-        params = dict(data=df,
-                      x=x,y=y,hue=hue,
-                      dodge=True)
-        sns.stripplot( size=8,
-                          jitter=0.35,
-                          palette='bright',
-                          edgecolor='black',
-                          linewidth=1,
-                          **params)
-        p_box = sns.boxplot(palette='pastel',linewidth=4, showmeans=showmeans,
-                            showfliers=False,**params)
-        if label_median :
+        import seaborn as sns
+        from pylab import (
+            axes,
+            boxplot,
+            figure,
+            legend,
+            plot,
+            savefig,
+            setp,
+            show,
+            xlim,
+            ylim,
+        )
+
+        plt.figure(figsize=cm2inch(figsize))
+        params = dict(data=df, x=x, y=y, hue=hue, dodge=True)
+        sns.stripplot(
+            size=8,
+            jitter=0.35,
+            palette="bright",
+            edgecolor="black",
+            linewidth=1,
+            **params
+        )
+        p_box = sns.boxplot(
+            palette="pastel",
+            linewidth=4,
+            showmeans=showmeans,
+            showfliers=False,
+            **params
+        )
+        if label_median:
             medians = df.groupby([x])[y].median()
-            maxes=df.groupby([x])[y].max()
-            vertical_offset = df[y].median() * 0.05 # offset from median for display
+            maxes = df.groupby([x])[y].max()
+            vertical_offset = df[y].median() * 0.05  # offset from median for display
             for xtick in p_box.get_xticks():
-                p_box.text(xtick,maxes[xtick]+vertical_offset,round(medians[xtick],3), 
-                        horizontalalignment='center',size='small',color='black',weight='semibold')
-        plt.ylabel(ylabel,size=10)
-        plt.xlabel('')
+                p_box.text(
+                    xtick,
+                    maxes[xtick] + vertical_offset,
+                    round(medians[xtick], 3),
+                    horizontalalignment="center",
+                    size="small",
+                    color="black",
+                    weight="semibold",
+                )
+        plt.ylabel(ylabel, size=10)
+        plt.xlabel("")
         # get legend information from the plot object
         handles, labels = p_box.get_legend_handles_labels()
         # specify just one legend
         plt.legend(handles[3:], labels[3:])
         plt.tight_layout()
-        plt.savefig(out,dpi=300)
-        
-        
-        
+        plt.savefig(out, dpi=300)
+
+
 class line_ploter:
-    
     def __init__(self):
 
         pass
-        
-    def line_plot(self,df,x,y,err,out,labs=[[None,None]],ylabel='',xlabel='',
-                     fnt_size=10,x_rotation=0,x_lim=(0,1),y_lim=(0,1),
-                     figsize = (8, 8)):
+
+    def line_plot(
+        self,
+        df,
+        x,
+        y,
+        err,
+        out,
+        labs=[[None, None]],
+        ylabel="",
+        xlabel="",
+        fnt_size=10,
+        x_rotation=0,
+        x_lim=(0, 1),
+        y_lim=(0, 1),
+        figsize=(8, 8),
+    ):
         """
-        
+
 
         Parameters
         ----------
@@ -142,43 +188,66 @@ class line_ploter:
         The line plots each for
 
         """
-        import pandas as pd
-        import numpy as np
         import matplotlib.pyplot as plt
-        from pylab import plot, show, savefig, xlim, figure,  ylim, legend, boxplot, setp, axes
+        import numpy as np
+        import pandas as pd
         import seaborn as sns
-        #sns.set(style='whitegrid', rc={"grid.linewidth": 0.1})
-        sns.set_context("paper" )            
+        from pylab import (
+            axes,
+            boxplot,
+            figure,
+            legend,
+            plot,
+            savefig,
+            setp,
+            show,
+            xlim,
+            ylim,
+        )
+
+        # sns.set(style='whitegrid', rc={"grid.linewidth": 0.1})
+        sns.set_context("paper")
         fig, ax = plt.subplots(figsize=cm2inch(figsize))
-        colors_=['r','b','g']
+        colors_ = ["r", "b", "g"]
         for index_ in range(len(y)):
-            y_=y[index_]
-            err_=err[index_]
-            x_=x
-            sns.lineplot(data=df,x=x_, y=y_, color=colors_[index_],
-                        label=r'AUC = %0.2f $\pm$ %0.2f' % (labs[index_][0],labs[index_][1]), 
-                        lw=2, alpha=.8)
+            y_ = y[index_]
+            err_ = err[index_]
+            x_ = x
+            sns.lineplot(
+                data=df,
+                x=x_,
+                y=y_,
+                color=colors_[index_],
+                label=r"AUC = %0.2f $\pm$ %0.2f" % (labs[index_][0], labs[index_][1]),
+                lw=2,
+                alpha=0.8,
+            )
             bound_up = np.minimum(np.array(df[y_]) + np.array(df[err_]), 1)
             bound_low = np.maximum(np.array(df[y_]) - np.array(df[err_]), 0)
-            ax.fill_between(np.array(df[x_]), bound_low, bound_up, color=colors_[index_], alpha=.1,
-                            #label=r'$\pm$ 1 std. dev.'
-                            )
+            ax.fill_between(
+                np.array(df[x_]),
+                bound_low,
+                bound_up,
+                color=colors_[index_],
+                alpha=0.1,
+                # label=r'$\pm$ 1 std. dev.'
+            )
         plt.setp(ax.get_legend().get_texts(), fontsize=fnt_size)
-        ax.tick_params(length=8,width=1)
-        plt.xticks(size=fnt_size) #9-10 points are standard paper size 
-        plt.xticks(rotation=x_rotation) 
+        ax.tick_params(length=8, width=1)
+        plt.xticks(size=fnt_size)  # 9-10 points are standard paper size
+        plt.xticks(rotation=x_rotation)
         plt.yticks(size=fnt_size)
         plt.ylim(y_lim)
         plt.xlim(x_lim)
-        plt.xlabel(xlabel,size=fnt_size)
-        plt.ylabel(ylabel,size=fnt_size)
+        plt.xlabel(xlabel, size=fnt_size)
+        plt.ylabel(ylabel, size=fnt_size)
         # Hide the right and top spines
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        
+        ax.spines["right"].set_visible(False)
+        ax.spines["top"].set_visible(False)
+
         # Only show ticks on the left and bottom spines
-        ax.yaxis.set_ticks_position('left')
-        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position("left")
+        ax.xaxis.set_ticks_position("bottom")
         plt.tight_layout()
-        
-        plt.savefig(out,dpi=300)
+
+        plt.savefig(out, dpi=300)
